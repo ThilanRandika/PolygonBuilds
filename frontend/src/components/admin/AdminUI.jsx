907c8833ebd09508e16b27840a2830db071110f9
Thilan Randika <dgtrandika@gmail.com>
2024-10-20 14:35:45 +0530

    basic frontend folder structure and add Tailwind CSS

---
+++ frontend/src/components/admin/AdminUI.jsx
@@ -0,0 +1,14 @@
+import Header from './header/Header'
+import Footer from './footer/Footer'
+
+function AdminUI() {
+  return (
+    <div>
+      <Header />
+      {/* Admin UI content */}
+      <Footer />
+    </div>
+  )
+}
+
+export default AdminUI
---
+++ frontend/src/components/customer/CustomerUI.jsx
@@ -0,0 +1,16 @@
+import Header from './header/Header'
+import Footer from './footer/Footer'
+
+function CustomerUI() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Customer UI</h1>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default CustomerUI
