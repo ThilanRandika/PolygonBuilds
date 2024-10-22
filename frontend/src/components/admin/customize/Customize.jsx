a091665ec7fcc220c1156230bffac76152772ba8
Thilan Randika <dgtrandika@gmail.com>
2024-10-23 00:34:07 +0530

    preview customizations

--- frontend/src/components/admin/customize/Customize.jsx
+++ frontend/src/components/admin/customize/Customize.jsx
@@ -1,9 +1,15 @@
 import AdminCustomizationForm from "./AdminCustomizationForm "
+import SelectionOptions from "../../customer/ModelRendering/SelectionOptions"
 
 function Customize() {
   return (
-    <div>
-      <AdminCustomizationForm></AdminCustomizationForm>
+    <div className="flex gap-5">
+      <div className="flex-1">
+        <AdminCustomizationForm></AdminCustomizationForm>
+      </div>
+      <div className="flex-1">
+        <SelectionOptions></SelectionOptions>
+      </div>
     </div>
   )
 }
