caad5a9016794e81fab3fcbc39940ad16e694be0
Thilan Randika <dgtrandika@gmail.com>
2024-12-08 18:26:50 +0530

    customer orders backend

--- frontend/src/components/customer/order/orders/Orders.jsx
+++ frontend/src/components/customer/order/orders/Orders.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { CircularProgress } from '@mui/material';
 
 function Orders() {
+  const UserID = 'userId';
+  const [orders, setOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await axios.get('http://localhost:8070/api/order/user-orders/${UserID}');
+        setOrders(response.data);
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+        setError('Failed to load orders.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
+  console.log('orders', orders);
+
+  if (isLoading) return <CircularProgress />; // Show loading spinner while fetching data
+
+  if (error) return <p>{error}</p>; // Show error message if there's an error
+
   return (
     <div>Orders </div>
   )
